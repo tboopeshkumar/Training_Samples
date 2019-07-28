274163b086025d93db77b7d7e8051d2e0e9eee6f
Boopesh <tboopeshkumar@gmail.com>
2019-07-28 23:13:13 +0530

    React Test Sample : Examples with Redux part 1

---
+++ Udemy_React_Testing/lootcheck/src/reducers/balance.js
@@ -0,0 +1,14 @@
+import * as constants from '../actions/constants';
+
+const balance = (state=0,action)=>{
+    switch(action.type){
+        case constants.SET_BALANCE :
+            return action.balance;
+        case constants.DEPOSIT : 
+            return state + action.deposit;
+        default:
+            return state;
+    }
+}
+
+export default balance;
