8397c0db99385463d3a2b09eb1c6e1179e1e1836
Boopesh <tboopeshkumar@gmail.com>
2019-08-22 09:58:49 +0530

    Updates

---
+++ precache-manifest.c3a332af0f83e560844985f78dca503f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26ae7bce1e1f4b5984d663b9b21de118",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a3f402cbbd4cb77a4ac",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "af302176bca6d934ecb1",
+    "url": "/static/js/2.9d9a4fbc.chunk.js"
+  },
+  {
+    "revision": "8a3f402cbbd4cb77a4ac",
+    "url": "/static/js/main.540ea920.chunk.js"
+  },
+  {
+    "revision": "889a859e9c4346196dca",
+    "url": "/static/js/runtime~main.aad2beca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
