2794719d51e9494c773460a4390cb5d09e7fd005
Boopesh <tboopeshkumar@gmail.com>
2019-08-22 09:58:12 +0530

    React 2019 : Github pages deploy script changes and small style improvements.

--- Udemy_React_2019/monsters-rolodex/src/App.js
+++ Udemy_React_2019/monsters-rolodex/src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
     return (
 
       <div className="App"> 
-        <h1>Monsters</h1>
+        <h1>Monsters Rolodex</h1>
         <SearchBox 
           placeholder="search monsters"
           handleChange={this.handleChange}/>
