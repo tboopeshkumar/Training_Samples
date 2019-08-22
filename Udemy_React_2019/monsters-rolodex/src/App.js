ea9f4343dcbfbbbcacef5f5bcb6c02ce95b39424
Boopesh <tboopeshkumar@gmail.com>
2019-08-22 09:44:41 +0530

    React 2019 : Monsters demo app : added search field and style changes

--- Udemy_React_2019/monsters-rolodex/src/App.js
+++ Udemy_React_2019/monsters-rolodex/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = { monsters: [] };
+    this.state = { monsters: [], searchField: '' };
   }
 
   componentDidMount() {
@@ -16,14 +17,25 @@ class App extends Component {
       })
   }
 
+  handleChange = e =>{
+    this.setState({ searchField: e.target.value});
+  }
+
   render() {
+
+    const { monsters, searchField } = this.state;
+    const fileteredMonsters = monsters.filter(monster => (
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    ));
+
     return (
 
-      <div className="App">
-        <CardList monsters={this.state.monsters}>
-         
-        </CardList>
-
+      <div className="App"> 
+        <h1>Monsters</h1>
+        <SearchBox 
+          placeholder="search monsters"
+          handleChange={this.handleChange}/>
+        <CardList monsters={fileteredMonsters} />
       </div>
     );
   }
